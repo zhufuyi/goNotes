ce12b5d853f416fbb9acafd5a73f7820eccda60f
zhufuyi <g.zhufuyi@gmail.com>
2016-07-11 23:20:09 +0800

    结构体类型

--- 1_type/4_struct/4_struct.go
+++ 1_type/4_struct/4_struct.go
@@ -1,6 +1,11 @@
+// 结构体的声明和使用
+
 /*
-Go语言的结构体（struct）和其他语言的类（class）有同等的地位，但Go语言放弃了包括继
-承在内的大量面向对象特性，只保留了组合（composition）这个最基础的特性。
+任何数据类型都可以作为结构体成员的类型，包括函数类型。
+
+从面相对象看，结构体成员是对象的属性，结构体关联的方法是对象的动作行为。
+
+结构体嵌入字段比接口嵌入类型更暴力，嵌入类型所有导出的成员和方法都无条件成为被嵌入结构体的成员和方法。
 */
 package main
 
@@ -8,56 +13,99 @@ import (
 	"fmt"
 )
 
-type Cuboid struct {
-	length, width, height float64
+// 人的信息
+type People struct {
+	Name    string // 名字
+	Gender  string // 性别
+	Age     uint8  // 年龄
+	Addr    string // 地址
+	*Family        // 家庭成员（嵌入字段）
+	*School        // 学校信息（嵌入字段）
 }
 
-func (a *Cuboid) Area() float64 { // 定义Cuboid结构体类型的方法Area，该方法接受者是Cuboid类型
-	return 2 * (a.length*a.height + a.length*a.width + a.width*a.height)
+// 人的睡觉行为
+func (ppl *People) Sleep() {
+	fmt.Println(ppl.Name + "每晚10点准时睡觉。")
 }
 
-func (v *Cuboid) Volume() (vl float64, err error) { // 定义Cuboid结构体类型的方法Area，该方法接受者是Cuboid类型，多个返回值写法
-	return v.length * v.height * v.length, err // 对应返回值个数
+// 人的吃饭行为
+func (ppl *People) Eat(name string) {
+	fmt.Println(ppl.Name + "和" + name + "在中秋一起吃团圆饭。")
 }
 
-type D2 struct {
-	x, y float64
+// 人的输出期末考试成绩行为
+func (ppl *People) GetScore() {
+	fmt.Println(ppl.School.GetScore())
 }
 
-type D3 struct {
-	D2
-	z float64
+// 家庭成员
+type Family struct {
+	Dad string
+	Mom string
 }
 
-func (m *D3) Mul() float64 { // 定义D3结构体类型的方法Mul，该方法接受者是D3类型
-	return m.x * m.y * m.z
+// 家庭成员的输出信息行为
+func (fml *Family) GetFamily() string {
+	return fmt.Sprintf("%s、%s", fml.Dad, fml.Mom)
+}
+
+// 家庭成员的吃饭行为
+func (fml *Family) Eat(name string) {
+	fmt.Println(fml.GetFamily() + "和" + name + "在中秋一起吃团圆饭。")
+}
+
+// 学校信息
+type School struct {
+	SchoolName  string
+	Headteacher string
+	Grade       string
+	*Score      // 成绩信息（嵌入字段）
+}
+
+// 学校输出信息行为
+func (sch *School) PrintInfo() string {
+	return fmt.Sprintf("学校：%d， 班主任：%d， 年级：%d， 分数：%s",
+		sch.SchoolName, sch.Headteacher, sch.Grade, sch.GetScore())
+}
+
+// 成绩信息
+type Score struct {
+	Chinese uint8
+	Math    uint8
+	English uint8
+	Music   uint8
+	Sports  uint8
+}
+
+// 成绩的输出信息行为
+func (sc *Score) GetScore() string {
+	return fmt.Sprintf("语文：%d， 数学：%d， 英语：%d， 音乐：%d， 体育：%d",
+		sc.Chinese, sc.Math, sc.English, sc.Music, sc.Sports)
 }
 
 func main() {
-	// 初始化方法
-	cb1 := new(Cuboid)
-	cb2 := &Cuboid{}
-	cb3 := &Cuboid{3, 4, 5}
-	cb4 := &Cuboid{length: 6, width: 7, height: 8} // 指向结构体的指针，函数传参时直接使用
-	cb5 := Cuboid{length: 6, width: 7, height: 8}  // 结构体，函数传参时前面需要加取址符号&
+	// 结构体实例化时比较常用方式的是指针方式，避免作为参数时拷贝整个结构体的副本
 
-	// 调用
-	cb1.length = 1 // 给结构体的元素赋值
-	cb1.width = 2
-	cb1.height = 3
-	fmt.Println("cb1 =", cb1)
-	fmt.Println("cb2 =", cb2)
-	ar := cb3.Area() // 结构体Cuboid的Area方法调用
-	fmt.Println("Area =", ar)
-	vl, _ := cb4.Volume() // 结构体Cuboid的Volume方法调用
-	fmt.Println("Volume =", vl)
-	fmt.Println("cb5", cb5)
+	sc := new(Score) // 用new创建
+	sc.Chinese = 98
+	sc.English = 90
+	sc.Math = 100
+	sc.Music = 88
+	sc.Sports = 99
 
-	// 结构体内置时可以直接外层结构体直接到具体元素，如果嵌入式的结构没有数据结构的名字 就默认是类型名字D2:D2
-	d1 := D3{D2: D2{x: 3, y: 4}, z: 5} // 初始化
-	d2 := D2{1, 2}                     // 初始化
-	d3 := D3{d2, 3}
-	fmt.Println("d1 =", d1, " d3 =", d3)
-	mul := d3.Mul()
-	fmt.Println("mul =", mul)
+	sch := &School{} // 用&创建
+	sch.SchoolName = "三国小学"
+	sch.Headteacher = "诸葛亮"
+	sch.Grade = "5(2)班"
+	sch.Score = sc
+
+	fml := &Family{"周瑜", "小乔"} // 用&创建并初始化
+
+	ppl := &People{"周循 ", "男", 11, "厦门", fml, sch} //实例化
+
+	// People的方法
+	ppl.Sleep()
+	ppl.Eat(ppl.GetFamily())
+	ppl.Family.Eat(ppl.Name)
+	ppl.GetScore()
 }
