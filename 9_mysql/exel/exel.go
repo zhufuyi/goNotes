8075dd1fbdd25036ef63472374b226078a129bc4
zhufuyi <g.zhufuyi@gmail.com>
2016-10-01 00:38:52 +0800

    添加插入表格语句

--- 9_mysql/exel/exel.go
+++ 9_mysql/exel/exel.go
@@ -7,19 +7,20 @@ import (
 	"strings"
 )
 
-// 卖家信息
-type Sellers struct {
-	SellerName string  // 卖家名称
-	ShopSite   string  // 卖家网址
-	Credit     int     // 信用值
-	Addr       string  // 卖家地址
-	Score      float64 // 店铺评分
+
+// 店铺信息
+type Shop struct {
+	ShopName string  // 店铺名称
+	ShopSite string  // 店铺网址
+	Credit   int     // 信用值
+	Addr     string  // 店铺地址
+	Score    float64 // 店铺评分
 }
 
 // 商品信息
 type Goods struct {
-	SellerId  int     // 卖家id
-	SellerNam string
+	ShopId    int     // 店铺id
+	ShopName  string  // 店铺名
 	MarketId  int     // 市场分类id
 	GoodsName string  // 商品名称
 	Praise    float64 // 好评度
@@ -27,7 +28,7 @@ type Goods struct {
 }
 
 type Exeler interface {
-	Extract() ([]*Sellers, []*Goods, error)
+	Extract() ([]*Shop, []*Goods, error)
 }
 
 func OpenExel(filename string) Exeler {
@@ -69,14 +70,14 @@ func (e *exelV)readContent(filename string) error {
 
 
 // 提取数据
-func (e *exelV)Extract() ([]*Sellers, []*Goods, error) {
+func (e *exelV)Extract() ([]*Shop, []*Goods, error) {
 	// 获取exel文件的内容
 	err := e.readContent(e.filename)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	var sellers []*Sellers
+	var shops []*Shop
 	var goods []*Goods
 
 	for n, s := range e.content {
@@ -85,7 +86,7 @@ func (e *exelV)Extract() ([]*Sellers, []*Goods, error) {
 			continue
 		}
 
-		sl := &Sellers{}
+		sl := &Shop{}
 		gd := &Goods{}
 		if len(s) == 10 {
 			credit := getCreditVal(s[3])
@@ -93,40 +94,39 @@ func (e *exelV)Extract() ([]*Sellers, []*Goods, error) {
 				continue
 			}
 			sl.Credit = credit
-			sl.SellerName = s[1]
+			sl.ShopName = s[1]
 			sl.ShopSite = s[2]
 			sl.Addr = s[6]
 			score, _ := strconv.ParseFloat(s[8], 32)
 			sl.Score = floatPointN(score, 2)
 
-			gd.SellerNam = s[1]
+			gd.ShopName = s[1]
 			//gd.GoodsName=s[4]
 			gd.GoodsName = strings.Replace(s[4], "'", `\'`, -1)
 			gd.Praise = cut2float(s[7])
 			gd.GoodsSite = s[9]
 		}
-		if sl.SellerName != ""&&sl.ShopSite != ""&&sl.Addr != "" {
+		if sl.ShopName != ""&&sl.ShopSite != ""&&sl.Addr != "" {
 			// 去掉空内容的行
-			sellers = append(sellers, sl)
+			shops = append(shops, sl)
 		}
 		if gd.GoodsName != ""&&gd.GoodsSite != "" {
 			// 去掉空内容的行
 			goods = append(goods, gd)
 		}
 	}
-
-	return sellers, goods, nil
+	return shops, goods, nil
 }
 
 
 
-// 获取卖家信用对应的序号
-func getCreditVal(CreditName string) int {
+// 获取店铺信用对应的序号
+func getCreditVal(cn string) int {
 	// 注：由于源文件的信用等级“天猫卖家”的值不唯一，通过这里统一值为21，id从1开始
-	redit := []string{"", "一星卖家", "二星卖家", "三星卖家", "四星卖家", "五星卖家", "一钻卖家", "二钻卖家", "三钻卖家", "四钻", "五钻卖家", "一蓝冠卖家", "二蓝冠卖家", "三蓝冠卖家", "四蓝冠卖家", "五蓝冠卖家", "一黄冠卖家", "二黄冠卖家", "三黄冠卖家", "四黄冠卖家", "五黄冠卖家", "天猫卖家"}
-	for index, v := range redit {
-		if v == CreditName {
-			return index
+	credit := []string{"一星卖家", "二星卖家", "三星卖家", "四星卖家", "五星卖家", "一钻卖家", "二钻卖家", "三钻卖家", "四钻", "五钻卖家", "一蓝冠卖家", "二蓝冠卖家", "三蓝冠卖家", "四蓝冠卖家", "五蓝冠卖家", "一黄冠卖家", "二黄冠卖家", "三黄冠卖家", "四黄冠卖家", "五黄冠卖家", "天猫卖家"}
+	for index, v := range credit {
+		if v == cn {
+			return index + 1
 		}
 	}
 	return -1
--- 9_mysql/taobao.go
+++ 9_mysql/taobao.go
@@ -1,44 +1,98 @@
-/*读取exel文件数据内容，把内容插入mysql数据库，并查询测试，注：exel数据文件来自网络*/
 package main
 
 import (
-	"fmt"
-	"./sql"
-	"strings"
-	"./filename"
 	"./exel"
+	"./filename"
+	"./sql"
+	"fmt"
+	"strings"
+	"time"
 )
 
 var X sql.Xer
 
+// 创建表
+func createTables() {
+	// 创建市场分类表
+	sql := `CREATE TABLE market(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(20) NOT NULL DEFAULT '' UNIQUE
+	)ENGINE myisam CHARSET utf8`
+	fmt.Println(X.Exec(sql))
+
+	// 创建信用等级表
+	sql = `CREATE TABLE credibility(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(20) NOT NULL DEFAULT '' UNIQUE
+	)ENGINE myisam CHARSET utf8`
+	fmt.Println(X.Exec(sql))
+
+	// 创建店铺信息表
+	sql = `CREATE TABLE shop(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT '' UNIQUE ,
+    addr VARCHAR(50) NOT NULL DEFAULT '',
+    shop_site VARCHAR(100) NOT NULL DEFAULT '' UNIQUE,
+    shop_credit TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    shop_score FLOAT NOT NULL DEFAULT 0.0,
+    KEY addr(addr),
+    KEY shop_credit(shop_credit),
+    KEY shop_score(shop_score)
+	)ENGINE myisam CHARSET utf8`
+	fmt.Println(X.Exec(sql))
+
+	// 创建商品信息表
+	sql = `CREATE TABLE goods(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    shop_id INT UNSIGNED NOT NULL DEFAULT 0,
+    market_id INT UNSIGNED NOT NULL DEFAULT 0,
+    goods_name CHAR(60) NOT NULL DEFAULT '',
+    goods_site VARCHAR(100) NOT NULL DEFAULT '' UNIQUE,
+    goods_praise FLOAT NOT NULL DEFAULT 0.0,
+    KEY shop_id(shop_id),
+    KEY market_id(market_id),
+    KEY goods_praise(goods_praise),
+    KEY goods_name(goods_name(10))
+	)ENGINE myisam CHARSET utf8`
+	fmt.Println(X.Exec(sql))
+}
 
 // 字符串拼接并执行sql语句
 func insertData(sql string, d []string) error {
 	for _, v := range d {
-		sql += "('" + v + "'),"    // 字符串拼接
+		sql += "('" + v + "')," // 字符串拼接
 	}
-	sql = strings.TrimRight(sql, ",")    // 去掉最后的逗号
+	sql = strings.TrimRight(sql, ",") // 去掉最后的逗号
 	_, err := X.Exec(sql)
 	return err
 }
 
-
-
 // 字符串拼接并执行sql语句2
 func insertData2(sql string, d map[string]string) error {
+	cnt := 1
+	str := ""
 	for _, v := range d {
-		sql += "(" + v + "),"    // 字符串拼接
+		cnt++
+		str += "(" + v + ")," // 字符串拼接
+		if cnt % 10 == 0 {
+			// 每次插入10行
+			str = strings.TrimRight(str, ",") // 去掉最后的逗号
+			_, err := X.Exec(sql + str)
+			if err != nil {
+				return err
+			}
+			str = ""
+		}
 	}
-	sql = strings.TrimRight(sql, ",")    // 去掉最后的逗号
-	_, err := X.Exec(sql)
-	return err
+
+	return nil
 }
 
 // 把exel文件内容插入数据库
 func insert2database() {
 	// 插入市场分类到数据库
 	func() {
-		sql := "INSERT INTO marketgroup (name) VALUES "
+		sql := "INSERT INTO market (name) VALUES "
 		market := []string{"服饰", "花鸟", "家电", "家居", "美妆", "母婴", "男包", "男鞋", "男装", "内衣", "女包", "女鞋", "女装", "配饰", "食品", "数码", "软件", "未分类"}
 		err := insertData(sql, market)
 		if err != nil {
@@ -46,9 +100,8 @@ func insert2database() {
 		} else {
 			fmt.Println("insert ok")
 		}
-		//fmt.Println(X.ExecString("SELECT * FROM marketgroup", "id", "name"))
+		//fmt.Println(X.ExecString("SELECT * FROM market", "id", "name"))
 	}()
-
 
 	// 插入信誉等级分类到数据库
 	func() {
@@ -66,35 +119,33 @@ func insert2database() {
 	var goods [][]*exel.Goods
 	sid := 0
 
-
-	// 插入卖家信息到数据库
+	// 插入店铺信息到数据库
 	func() {
 		market := []string{"服饰", "花鸟", "家电", "家居", "美妆", "母婴", "男包", "男鞋", "男装", "内衣", "女包", "女鞋", "女装", "配饰", "食品", "数码", "软件", "未分类"}
 
 		// 读取文件列表
-		f := filename.Getfiles("./淘宝卖家数据库")
+		f := filename.Getfiles("./淘宝数据")
 		files, err := f.GetExelFiles()
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		//fmt.Println(exels)
 
-		sellers := make(map[string]string)
+		shops := make(map[string]string)
 		for _, fileName := range files {
-			seller, gds, err := exel.OpenExel(fileName).Extract()
+			shps, gds, err := exel.OpenExel(fileName).Extract()
 			if err != nil {
 				fmt.Println(err.Error())
 				continue
 			}
 
-			s2i := make(map[string]int)
-			for _, sl := range seller {
-				if _, ok := sellers[sl.SellerName]; !ok {
+			s2i := make(map[string]int) // 关联shop名称和shopid
+			for _, sl := range shps {
+				if _, ok := shops[sl.ShopName]; !ok {
 					// 不存在则写入
 					sid++
-					s2i[sl.SellerName] = sid
-					sellers[sl.SellerName] = fmt.Sprintf(" '%d','%s','%s','%s','%d','%f'", sid, sl.SellerName, sl.Addr, sl.ShopSite, sl.Credit, sl.Score)
+					s2i[sl.ShopName] = sid
+					shops[sl.ShopName] = fmt.Sprintf(" '%d','%s','%s','%s','%d','%f'", sid, sl.ShopName, sl.Addr, sl.ShopSite, sl.Credit, sl.Score)
 				}
 			}
 
@@ -102,8 +153,8 @@ func insert2database() {
 				// 通过文件名来查找市场分类的序号
 				if strings.Contains(fileName, mk) {
 					for _, gd := range gds {
-						gd.MarketId = mid + 1    // 补充该商品属于哪个市场分类，id从1开始
-						gd.SellerId = s2i[gd.SellerNam]
+						gd.MarketId = mid + 1 // 补充该商品属于哪个市场分类，id从1开始
+						gd.ShopId = s2i[gd.ShopName]
 					}
 					break
 				}
@@ -112,108 +163,140 @@ func insert2database() {
 			goods = append(goods, gds)
 		}
 
-		sql := "INSERT INTO seller (id,name,addr,shop_site,credit_val,shop_score) VALUES "
-		err = insertData2(sql, sellers)
+		sql := "INSERT INTO shop (id,name,addr,shop_site,shop_credit,shop_score) VALUES "
+		err = insertData2(sql, shops)
 		if err != nil {
 			fmt.Println(err.Error())
 		} else {
 			fmt.Println("insert ok!")
 		}
-		//fmt.Println(X.ExecString("SELECT count(*) FROM seller", "count(*)"))
+		//fmt.Println(X.ExecString("SELECT count(*) FROM shop", "count(*)"))
 	}()
-
 
 	// 插入商品信息到数据库
 	func() {
 		goodsmap := make(map[string]string)
 		for _, gds := range goods {
 			for _, gd := range gds {
-				goodsmap[gd.GoodsSite] = fmt.Sprintf(" '%d','%d','%s','%s','%f'", gd.SellerId, gd.MarketId, gd.GoodsName, gd.GoodsSite, gd.Praise)
+				goodsmap[gd.GoodsSite] = fmt.Sprintf(" '%d','%d','%s','%s','%f'", gd.ShopId, gd.MarketId, gd.GoodsName, gd.GoodsSite, gd.Praise)
 			}
 		}
 
-		sql := "INSERT INTO goods (seller_id,market_id,goods_name,goods_site,goods_praise) VALUES "
+		sql := "INSERT INTO goods (shop_id,market_id,goods_name,goods_site,goods_praise) VALUES "
 		err := insertData2(sql, goodsmap)
 		if err != nil {
 			fmt.Println(err.Error())
 		} else {
 			fmt.Println("insert ok!")
 		}
-		//fmt.Println(X.ExecString("SELECT count(*) FROM seller", "count(*)"))
+		//fmt.Println(X.ExecString("SELECT count(*) FROM shop", "count(*)"))
 
 	}()
 }
-
 
 // 测试查询
 func testQuery() {
 	var sql string
 	var rs [][]string
 
-	// (1) 查看一共有多少个卖家
-	fmt.Println("\n\n-------------查看一共有多少个卖家-----------------")
-	sql = "SELECT count(*) FROM seller"
-	rs, _ = X.ExecString(sql)
-	fmt.Println("卖家数量 =", rs[0][0])
+	// (1) 查看一共有多少个店铺
+	fmt.Println("\n\n-------------查看一共有多少个店铺-----------------")
+	t := time.Now()
+	func() {
+		sql = "SELECT count(*) FROM shop"
+		rs, _ = X.ExecString(sql)
+		fmt.Println("店铺数量 =", rs[0][0])
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
 	// (2) 查看一共有多少个商品
 	fmt.Println("\n\n-------------查看一共有多少个商品-----------------")
-	sql = "SELECT count(*) FROM goods"
-	rs, _ = X.ExecString(sql)
-	fmt.Println("商品数量 =", rs[0][0])
+	t = time.Now()
+	func() {
+		sql = "SELECT count(*) FROM goods"
+		rs, _ = X.ExecString(sql)
+		fmt.Println("商品数量 =", rs[0][0])
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
-	// (3) 查看有多少个天猫卖家
-	fmt.Println("\n\n--------------查看有多少个天猫卖家----------------")
-	sql = "SELECT count(*) FROM seller WHERE credit_val = (SELECT id FROM credibility WHERE name='天猫')"
-	rs, _ = X.ExecString(sql)
-	fmt.Println("天猫卖家数量 =", rs[0][0])
+	// (3) 查看有多少个天猫店铺
+	fmt.Println("\n\n--------------查看有多少个天猫店铺----------------")
+	t = time.Now()
+	func() {
+		sql = "SELECT count(*) FROM shop WHERE shop_credit = (SELECT id FROM credibility WHERE name='天猫')"
+		rs, _ = X.ExecString(sql)
+		fmt.Println("天猫店铺数量 =", rs[0][0])
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
-	// (4) 查找地址在广州并且店铺评分大于4.95分的皇冠以上卖家
-	fmt.Println("\n\n--------------查找地址在广州并且店铺评分大于4.95分的皇冠以上卖家----------------")
-	sql = "SELECT * FROM seller WHERE (addr REGEXP '广州') AND (shop_score>4.95) AND (credit_val>(SELECT id FROM credibility WHERE name='一黄冠'))"
-	rs, _ = X.ExecString(sql, "name", "addr", "credit_val", "shop_score")
-	for _, v := range rs {
-		fmt.Println(v)
-	}
+	// (4) 查找地址在广州并且店铺评分至少4.95分的皇冠以上店铺名称
+	fmt.Println("\n\n--------------查找地址在广州并且店铺评分至少4.95分的皇冠以上店铺名称----------------")
+	t = time.Now()
+	func() {
+		sql = "SELECT * FROM shop WHERE (addr REGEXP '广州') AND (shop_score>=4.949) AND (shop_credit>=(SELECT id FROM credibility WHERE name='一黄冠'))"
+		rs, _ = X.ExecString(sql, "name")
+		for _, v := range rs {
+			fmt.Println(v[0])
+		}
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
-	// (5) 查询好评率大于98%的手机相关商品
-	fmt.Println("\n\n--------------查询好评率大于98%的手机商品----------------")
-	sql = "SELECT * FROM goods WHERE (goods_name REGEXP '手机') AND (goods_praise>98) AND (market_id=(SELECT id FROM marketgroup WHERE name='数码'))"
-	rs, _ = X.ExecString(sql, "goods_name", "goods_praise")
-	for _, v := range rs {
-		fmt.Println(v)
-	}
+	// (5) 查询好评率99%以上的手机相关商品的网址
+	fmt.Println("\n\n--------------查看好评率99%以上的手机相关商品的网址----------------")
+	t = time.Now()
+	func() {
+		sql = "SELECT * FROM goods WHERE (goods_name REGEXP '手机') AND (goods_praise>=99)"
+		rs, _ = X.ExecString(sql, "goods_site")
+		for _, v := range rs {
+			fmt.Println(v[0])
+		}
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
-	// (6) 查询至少有6个商品以上的卖家信息
-	fmt.Println("\n\n--------------查询至少有6个商品以上的卖家信息----------------")
-	sql = "SELECT * FROM seller RIGHT JOIN (SELECT seller_id,count(*) AS cnt FROM goods GROUP BY seller_id HAVING cnt >=6) AS multigoods ON seller.id=multigoods.seller_id HAVING multigoods.seller_id>0"
-	rs, _ = X.ExecString(sql, "id", "name", "addr", "credit_val", "shop_score", "cnt")
-	for _, v := range rs {
-		fmt.Println(v)
-	}
+	// (6) 查询至少有6个商品以上的店铺名称
+	fmt.Println("\n\n--------------查询至少有6个商品以上的店铺名称----------------")
+	t = time.Now()
+	func() {
+		sql = "SELECT * FROM shop RIGHT JOIN (SELECT shop_id,count(*) AS cnt FROM goods GROUP BY shop_id HAVING cnt >=6) AS multigoods ON shop.id=multigoods.shop_id HAVING multigoods.shop_id>0"
+		rs, _ = X.ExecString(sql, "name")
+		for _, v := range rs {
+			fmt.Println(v[0])
+		}
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 
-	// (7)查询各市场分类中好评度最好的商品的卖家名称
-	fmt.Println("\n\n--------------查询各市场分类中好评度最好的商品的卖家名称----------------")
-	sql = "SELECT name FROM seller RIGHT JOIN (SELECT seller_id,max(goods_praise) FROM goods GROUP BY market_id) AS bestgoods ON seller.id=bestgoods.seller_id"
-	rs, _ = X.ExecString(sql, "id", "name", "addr", "credit_val", "shop_score", "cnt")
-	for _, v := range rs {
-		fmt.Println(v)
-	}
-
+	// (7)查询各市场分类中好评度最好最新的商品的店铺名称
+	fmt.Println("\n\n--------------查询各市场分类中好评度最好最新的商品的店铺名称----------------")
+	t = time.Now()
+	func() {
+		sql = `SELECT market.name,bestshop.name FROM market RIGHT JOIN
+    (SELECT id,name,market_id FROM shop RIGHT JOIN
+        (SELECT shop_id,market_id FROM goods WHERE id IN (SELECT max(id) FROM goods WHERE goods_praise>=100 GROUP BY market_id)
+        ) AS bestgoods ON shop.id=bestgoods.shop_id) AS bestshop
+    ON market.id=bestshop.market_id`
+		rs, _ = X.ExecString(sql, "name")
+		for _, v := range rs {
+			fmt.Println(v[0])
+		}
+	}()
+	fmt.Println("使用时间(ns)：", time.Now().Sub(t).Nanoseconds())
 }
 
 func main() {
-	// 连接数据库
+	// (1)连接数据库
 	var err error
-						//驱动名   用户名	密码		主机	  端口	 数据库名	
-	X, err = sql.NewXorm("mysql", "root", "123456", "localhost", "3306", "taobao")
+	X, err = sql.NewXorm("mysql", "root", "123456", "192.168.8.102", "3306", "taobao")
 	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 
-	// 插入数据
-	insert2database()
+	// (2)创建表
+	//createTables()
 
-	// 测试查询
+	// (3)插入数据
+	//insert2database()
+
+	// (4)测试查询
 	testQuery()
 }
